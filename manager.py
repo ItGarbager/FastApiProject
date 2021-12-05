7c7cd5638277a37ed6f5819f8a4315fb9fd4a0a7
ItGarbager <95277218+ItGarbager@users.noreply.github.com>
2021-12-05 17:16:09 +0800

    Update manager.py

--- manager.py
+++ manager.py
@@ -5,6 +5,6 @@ from FastApiProject.config import APP_RUN_CONFIG
 app = CreateApp()
 
 if __name__ == "__main__":
-    # 启动服务，因为我们这个文件叫做 manager.py，所以需要启动 manager.py 里面的 app_1
-    # 第一个参数 "main:app_1" 就表示这个含义，然后是 host 和 port 表示监听的 ip 和端口
+    # 启动服务，因为我们这个文件叫做 manager.py，所以需要启动 manager.py 里面的 app
+    # 第一个参数 "main:app" 就表示这个含义，然后是 host 和 port 表示监听的 ip 和端口
     uvicorn.run("manager:app", **APP_RUN_CONFIG)
